17b8f080d7107f6b1e57392218acc967c338a489
dredavidOps <drewizzly@gmail.com>
2024-08-21 01:08:41 +0200

    re-wrote clean_environment

--- clean_environment.py
+++ clean_environment.py
@@ -1,4 +1,9 @@
+from flask import Flask
 import requests
+
+app = Flask(__name__)
+
+users = {}
 
 requests.get('http://127.0.0.1:5000/stop_server')
 requests.get('http://127.0.0.1:5001/stop_server')
