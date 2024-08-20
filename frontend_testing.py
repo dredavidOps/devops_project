017e5a5d7a289a506e95a6a6e94e22cc0e5376a4
dredavidOps <drewizzly@gmail.com>
2024-08-21 01:01:27 +0200

    cleaned up comment in frontend_testing

--- frontend_testing.py
+++ frontend_testing.py
@@ -8,9 +8,6 @@ driver = webdriver.Chrome(service=Service(""))
 driver.get('http://127.0.0.1:5001/users/4')
 
 my_data = driver.find_element(By.ID, 'user')
-# driver.find_element(By.CLASS_NAME, value='er8xn').click()
-# driver.find_element(By.CLASS_NAME, value='er8xn').send_keys("hello")
-# driver.find_element(By.CLASS_NAME, value="blockInner")
 
 time.sleep(100)
 print(my_data)
